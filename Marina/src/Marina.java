6380698555fe49404779b5ae030d1f642786c286
root <root@Marina-Viahub>
2017-02-24 12:27:13 +0100

    second commit

--- Marina/src/Marina.java
+++ Marina/src/Marina.java
@@ -3,11 +3,13 @@ public class Marina {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		System.out.println("1");
 		int x=1;
 		int y=2;
 		int z=x+y;
 		System.out.println(z);
+
 	}
 
 }
